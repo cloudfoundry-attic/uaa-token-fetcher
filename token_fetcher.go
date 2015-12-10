3e97a1c5ffdc0ca00b9361ecd61eabb187a22fc5
Atul Kshirsagar <atul.kshirsagar@gmail.com>
2015-12-09 17:01:54 -0800

    WIP
    Signed-off-by: Fermin Ordaz <fermin.ordaz@gmail.com>

--- token_fetcher.go
+++ token_fetcher.go
@@ -8,7 +8,11 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"sync"
+	"time"
+
 	trace "github.com/cloudfoundry-incubator/trace-logger"
+	"github.com/pivotal-golang/clock"
 )
 
 type OAuthConfig struct {
@@ -18,66 +22,156 @@ type OAuthConfig struct {
 	Port          int    `yaml:"port"`
 }
 
+type TokenFetcherConfig struct {
+	MaxNumberOfRetries   uint32
+	RetryInterval        time.Duration
+	ExpirationBufferTime int64
+}
+
 type TokenFetcher interface {
-	FetchToken() (*Token, error)
+	FetchToken(useCachedToken bool) (*Token, error)
 }
 
 type Token struct {
 	AccessToken string `json:"access_token"`
-	ExpireTime  int    `json:"expires_in"`
+	// Expire time in seconds
+	ExpireTime int64 `json:"expires_in"`
 }
 
 type Fetcher struct {
-	config *OAuthConfig
-	client *http.Client
+	clock              clock.Clock
+	config             *OAuthConfig
+	client             *http.Client
+	tokenFetcherConfig TokenFetcherConfig
+	cachedToken        *Token
+	refetchTokenTime   int64
+	lock               *sync.Mutex
 }
 
-func NewTokenFetcher(config *OAuthConfig) *Fetcher {
-	return &Fetcher{
-		config: config,
-		client: &http.Client{},
+func NewTokenFetcher(config *OAuthConfig, tokenFetcherConfig TokenFetcherConfig, clock clock.Clock) (TokenFetcher, error) {
+	if config == nil {
+		return nil, errors.New("OAuth configuration cannot be nil")
 	}
+
+	if config.Port <= 0 || config.Port > 65535 {
+		return nil, errors.New("OAuth port is not in valid range 1-65535")
+	}
+
+	if config.ClientName == "" {
+		return nil, errors.New("OAuth Client ID cannot be empty")
+	}
+
+	if config.ClientSecret == "" {
+		return nil, errors.New("OAuth Client Secret cannot be empty")
+	}
+
+	if config.TokenEndpoint == "" {
+		return nil, errors.New("OAuth Token endpoint cannot be empty")
+	}
+
+	if tokenFetcherConfig.MaxNumberOfRetries == 0 {
+		return nil, errors.New("Max number of retries cannot be zero")
+	}
+
+	if tokenFetcherConfig.ExpirationBufferTime < 0 {
+		return nil, errors.New("Expiration buffer time cannot be negative")
+	}
+
+	return &Fetcher{
+		config:             config,
+		client:             &http.Client{},
+		tokenFetcherConfig: tokenFetcherConfig,
+		clock:              clock,
+		lock:               new(sync.Mutex),
+	}, nil
 }
 
-func (f *Fetcher) FetchToken() (*Token, error) {
+func (f *Fetcher) FetchToken(useCachedToken bool) (*Token, error) {
+	f.lock.Lock()
+	defer f.lock.Unlock()
+
+	if useCachedToken && f.canReturnCachedToken() {
+		return f.cachedToken, nil
+	}
+
+	retry := true
+	var retryCount uint32 = 1
+	var token *Token
+	for retry == true {
+		fmt.Println("Calling doFetch")
+		token, retry, err := f.doFetch()
+		fmt.Printf("RETRY:%t, RETRY COUNT:%d\n", retry, retryCount)
+		if token != nil {
+			break
+		}
+		if retry && retryCount < f.tokenFetcherConfig.MaxNumberOfRetries {
+			fmt.Printf("SLEEPING BEFORE RETRYING...\n")
+			retryCount++
+			f.clock.Sleep(f.tokenFetcherConfig.RetryInterval)
+			fmt.Printf("WOKE UP...\n")
+			continue
+		} else {
+			return nil, err
+		}
+	}
+
+	f.updateCachedToken(token)
+	return token, nil
+}
+
+func (f *Fetcher) doFetch() (*Token, bool, error) {
 	values := url.Values{}
 	values.Add("grant_type", "client_credentials")
 	requestBody := values.Encode()
-
 	tokenURL := fmt.Sprintf("%s:%d/oauth/token", f.config.TokenEndpoint, f.config.Port)
 	request, err := http.NewRequest("POST", tokenURL, bytes.NewBuffer([]byte(requestBody)))
 	if err != nil {
-		return nil, err
+		return nil, false, err
 	}
-
-	trace.DumpRequest(request)
 
 	request.SetBasicAuth(f.config.ClientName, f.config.ClientSecret)
 	request.Header.Add("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
 	request.Header.Add("Accept", "application/json; charset=utf-8")
-
+	trace.DumpRequest(request)
+	fmt.Println("Calling http client do")
 	resp, err := f.client.Do(request)
 	if err != nil {
-		return nil, err
+		fmt.Printf("Error from oauth server:%#v\n", err)
+		return nil, true, err
 	}
+	fmt.Println("Done calling oauth server")
 	defer resp.Body.Close()
 
 	trace.DumpResponse(resp)
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return nil, true, err
 	}
 
 	if resp.StatusCode != http.StatusOK {
-		return nil, errors.New(fmt.Sprintf("status code: %d, body: %s", resp.StatusCode, body))
+		retry := false
+		fmt.Printf("Status Code:%d\n", resp.StatusCode)
+		if resp.StatusCode >= http.StatusInternalServerError {
+			retry = true
+		}
+		fmt.Printf("Retry:%t\n", retry)
+		return nil, retry, errors.New(fmt.Sprintf("status code: %d, body: %s", resp.StatusCode, body))
 	}
 
 	token := &Token{}
 	err = json.Unmarshal(body, token)
 	if err != nil {
-		return nil, err
+		return nil, false, err
 	}
+	return token, false, nil
+}
 
-	return token, nil
+func (f *Fetcher) canReturnCachedToken() bool {
+	return f.cachedToken != nil && f.clock.Now().Unix() < f.refetchTokenTime
+}
+
+func (f *Fetcher) updateCachedToken(token *Token) {
+	f.cachedToken = token
+	f.refetchTokenTime = f.clock.Now().Unix() + (token.ExpireTime - f.tokenFetcherConfig.ExpirationBufferTime)
 }
--- token_fetcher_test.go
+++ token_fetcher_test.go
@@ -1,13 +1,17 @@
 package token_fetcher_test
 
 import (
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strconv"
 	"strings"
+	"sync"
+	"time"
 
 	. "github.com/cloudfoundry-incubator/uaa-token-fetcher"
+	"github.com/pivotal-golang/clock/fakeclock"
 
 	"bytes"
 
@@ -28,12 +32,22 @@ var verifyBody = func(expectedBody string) http.HandlerFunc {
 }
 
 var _ = Describe("TokenFetcher", func() {
+	const (
+		DefaultMaxNumberOfRetries   = 3
+		DefaultRetryInterval        = 15 * time.Second
+		DefaultExpirationBufferTime = 30
+	)
+
 	var (
-		cfg    *OAuthConfig
-		server *ghttp.Server
+		cfg                *OAuthConfig
+		server             *ghttp.Server
+		clock              *fakeclock.FakeClock
+		tokenFetcherConfig TokenFetcherConfig
+		canUseCachedToken  bool
 	)
 
 	BeforeEach(func() {
+		canUseCachedToken = true
 		cfg = &OAuthConfig{}
 		server = ghttp.NewServer()
 
@@ -48,100 +62,445 @@ var _ = Describe("TokenFetcher", func() {
 
 		cfg.ClientName = "client-name"
 		cfg.ClientSecret = "client-secret"
+		clock = fakeclock.NewFakeClock(time.Now())
+		tokenFetcherConfig = TokenFetcherConfig{
+			MaxNumberOfRetries:   DefaultMaxNumberOfRetries,
+			RetryInterval:        DefaultRetryInterval,
+			ExpirationBufferTime: DefaultExpirationBufferTime,
+		}
 	})
 
 	AfterEach(func() {
 		server.Close()
 	})
 
-	Describe(".NewTokenFetcher", func() {
-		It("maybe does something interesting", func() {})
+	verifyLogs := func(stdout *bytes.Buffer, reqMessage, resMessage string) {
+		r, err := ioutil.ReadAll(stdout)
+		log := string(r)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(log).To(ContainSubstring("REQUEST: "))
+		Expect(log).To(ContainSubstring(reqMessage))
+
+		Expect(log).To(ContainSubstring("RESPONSE: "))
+		Expect(log).To(ContainSubstring(resMessage))
+	}
+
+	getOauthHandlerFunc := func(status int, token *Token) http.HandlerFunc {
+		return ghttp.CombineHandlers(
+			ghttp.VerifyRequest("POST", "/oauth/token"),
+			ghttp.VerifyBasicAuth("client-name", "client-secret"),
+			ghttp.VerifyContentType("application/x-www-form-urlencoded; charset=UTF-8"),
+			ghttp.VerifyHeader(http.Header{
+				"Accept": []string{"application/json; charset=utf-8"},
+			}),
+			verifyBody("grant_type=client_credentials"),
+			ghttp.RespondWithJSONEncoded(status, token),
+		)
+	}
+
+	verifyFetchWithRetries := func(fetcher TokenFetcher, stdout *bytes.Buffer, server *ghttp.Server, expectedResponses ...string) {
+		wg := sync.WaitGroup{}
+		wg.Add(1)
+		go func(wg *sync.WaitGroup) {
+			defer GinkgoRecover()
+			defer wg.Done()
+			_, err := fetcher.FetchToken(canUseCachedToken)
+			Expect(err).To(HaveOccurred())
+		}(&wg)
+
+		for i := 0; i < DefaultMaxNumberOfRetries; i++ {
+			fmt.Printf("Trying %d times\n", i)
+			Eventually(server.ReceivedRequests, 5*time.Second, 1*time.Second).Should(HaveLen(i + 1))
+			clock.Increment(DefaultRetryInterval + 10*time.Second)
+		}
+
+		r, err := ioutil.ReadAll(stdout)
+		Expect(err).NotTo(HaveOccurred())
+		log := string(r)
+
+		for _, respMessage := range expectedResponses {
+			Expect(log).To(ContainSubstring(respMessage))
+		}
+
+		wg.Wait()
+	}
+	Describe("NewTokenFetcher", func() {
+		Context("when all values are valid", func() {
+			It("returns a token fetcher instance", func() {
+				tokenFetcher, err := NewTokenFetcher(cfg, tokenFetcherConfig, clock)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(tokenFetcher).NotTo(BeNil())
+			})
+		})
+
+		Context("when values are invalid", func() {
+			Context("when oauth config is nil", func() {
+				It("returns error", func() {
+					tokenFetcher, err := NewTokenFetcher(nil, tokenFetcherConfig, clock)
+					Expect(err).To(HaveOccurred())
+					Expect(err.Error()).To(ContainSubstring("OAuth configuration cannot be nil"))
+					Expect(tokenFetcher).To(BeNil())
+				})
+			})
+
+			Context("when oauth config port is not in range", func() {
+				It("returns error", func() {
+					oauthConfig := &OAuthConfig{
+						TokenEndpoint: "http://some.url",
+						ClientName:    "client-name",
+						ClientSecret:  "client-secret",
+						Port:          -1,
+					}
+					tokenFetcher, err := NewTokenFetcher(oauthConfig, tokenFetcherConfig, clock)
+					Expect(err).To(HaveOccurred())
+					Expect(err.Error()).To(ContainSubstring("OAuth port is not in valid range 1-65535"))
+					Expect(tokenFetcher).To(BeNil())
+				})
+			})
+
+			Context("when oauth config client id is empty", func() {
+				It("returns error", func() {
+					oauthConfig := &OAuthConfig{
+						TokenEndpoint: "http://some.url",
+						ClientName:    "",
+						ClientSecret:  "client-secret",
+						Port:          8080,
+					}
+					tokenFetcher, err := NewTokenFetcher(oauthConfig, tokenFetcherConfig, clock)
+					Expect(err).To(HaveOccurred())
+					Expect(err.Error()).To(ContainSubstring("OAuth Client ID cannot be empty"))
+					Expect(tokenFetcher).To(BeNil())
+				})
+			})
+
+			Context("when oauth config client secret is empty", func() {
+				It("returns error", func() {
+					oauthConfig := &OAuthConfig{
+						TokenEndpoint: "http://some.url",
+						ClientName:    "client-name",
+						ClientSecret:  "",
+						Port:          8080,
+					}
+					tokenFetcher, err := NewTokenFetcher(oauthConfig, tokenFetcherConfig, clock)
+					Expect(err).To(HaveOccurred())
+					Expect(err.Error()).To(ContainSubstring("OAuth Client Secret cannot be empty"))
+					Expect(tokenFetcher).To(BeNil())
+				})
+			})
+
+			Context("when oauth config tokenendpoint is empty", func() {
+				It("returns error", func() {
+					oauthConfig := &OAuthConfig{
+						TokenEndpoint: "",
+						ClientName:    "client-name",
+						ClientSecret:  "client-secret",
+						Port:          8080,
+					}
+					tokenFetcher, err := NewTokenFetcher(oauthConfig, tokenFetcherConfig, clock)
+					Expect(err).To(HaveOccurred())
+					Expect(err.Error()).To(ContainSubstring("OAuth Token endpoint cannot be empty"))
+					Expect(tokenFetcher).To(BeNil())
+				})
+			})
+
+			Context("when token fetcher config's max number of retries is zero", func() {
+				It("returns error", func() {
+					tokenFetcherCfg := TokenFetcherConfig{
+						MaxNumberOfRetries:   0,
+						RetryInterval:        2 * time.Second,
+						ExpirationBufferTime: 30,
+					}
+					tokenFetcher, err := NewTokenFetcher(cfg, tokenFetcherCfg, clock)
+					Expect(err).To(HaveOccurred())
+					Expect(err.Error()).To(ContainSubstring("Max number of retries cannot be zero"))
+					Expect(tokenFetcher).To(BeNil())
+				})
+			})
+
+			Context("when token fetcher config's expiration buffer time is negative", func() {
+				It("returns error", func() {
+					tokenFetcherCfg := TokenFetcherConfig{
+						MaxNumberOfRetries:   3,
+						RetryInterval:        2 * time.Second,
+						ExpirationBufferTime: -1,
+					}
+					tokenFetcher, err := NewTokenFetcher(cfg, tokenFetcherCfg, clock)
+					Expect(err).To(HaveOccurred())
+					Expect(err.Error()).To(ContainSubstring("Expiration buffer time cannot be negative"))
+					Expect(tokenFetcher).To(BeNil())
+				})
+			})
+		})
 	})
 
-	Describe(".FetchToken", func() {
-		Context("when OAuth server cannot be reached", func() {
-			It("returns an error", func() {
-				cfg.TokenEndpoint = "http://bogus.url"
-				fetcher := NewTokenFetcher(cfg)
-				_, err := fetcher.FetchToken()
-				Expect(err).To(HaveOccurred())
-			})
-		})
+	Describe("FetchToken", func() {
+		var (
+			tokenFetcher TokenFetcher
+		)
 
-		Context("when the respose body is malformed", func() {
-			It("returns an error", func() {
-				server.AppendHandlers(
-					ghttp.RespondWithJSONEncoded(http.StatusOK, "broken garbage response"),
-				)
-
-				fetcher := NewTokenFetcher(cfg)
-				_, err := fetcher.FetchToken()
-				Expect(err).To(HaveOccurred())
-				Expect(server.ReceivedRequests()).Should(HaveLen(1))
-			})
-		})
-
-		Context("when a non 200 OK is returned", func() {
-			It("returns an error", func() {
-				server.AppendHandlers(
-					ghttp.RespondWith(http.StatusBadRequest, "you messed up"),
-				)
-
-				fetcher := NewTokenFetcher(cfg)
-				_, err := fetcher.FetchToken()
-				Expect(err).To(HaveOccurred())
-				Expect(err.Error()).To(Equal("status code: 400, body: you messed up"))
-				Expect(server.ReceivedRequests()).Should(HaveLen(1))
-			})
-		})
-
-		It("returns a new token", func() {
-			responseBody := &Token{
-				AccessToken: "the token",
-				ExpireTime:  20,
-			}
-
-			server.AppendHandlers(
-				ghttp.CombineHandlers(
-					ghttp.VerifyRequest("POST", "/oauth/token"),
-					ghttp.VerifyBasicAuth("client-name", "client-secret"),
-					ghttp.VerifyContentType("application/x-www-form-urlencoded; charset=UTF-8"),
-					ghttp.VerifyHeader(http.Header{
-						"Accept": []string{"application/json; charset=utf-8"},
-					}),
-					verifyBody("grant_type=client_credentials"),
-					ghttp.RespondWithJSONEncoded(http.StatusOK, responseBody),
-				))
-
-			fetcher := NewTokenFetcher(cfg)
-			token, err := fetcher.FetchToken()
+		BeforeEach(func() {
+			var err error
+			tokenFetcher, err = NewTokenFetcher(cfg, tokenFetcherConfig, clock)
 			Expect(err).NotTo(HaveOccurred())
-			Expect(server.ReceivedRequests()).Should(HaveLen(1))
-			Expect(token.AccessToken).To(Equal("the token"))
-			Expect(token.ExpireTime).To(Equal(20))
+			Expect(tokenFetcher).NotTo(BeNil())
 		})
 
-		It("logs requests and responses", func() {
-			stdout := bytes.NewBuffer([]byte{})
-			trace.SetStdout(stdout)
-			trace.NewLogger("true")
+		Context("multiple retries", func() {
+			Context("when OAuth server cannot be reached", func() {
+				It("returns an error", func() {
+					stdout := bytes.NewBuffer([]byte{})
+					trace.SetStdout(stdout)
+					trace.NewLogger("true")
 
-			server.AppendHandlers(
-				ghttp.RespondWith(http.StatusBadRequest, "you messed up"),
-			)
+					cfg.TokenEndpoint = "http://bogus.url"
+					fetcher, err := NewTokenFetcher(cfg, tokenFetcherConfig, clock)
+					Expect(err).NotTo(HaveOccurred())
+					wg := sync.WaitGroup{}
+					wg.Add(1)
+					go func(wg *sync.WaitGroup) {
+						defer GinkgoRecover()
+						defer wg.Done()
+						_, err := fetcher.FetchToken(canUseCachedToken)
+						Expect(err).To(HaveOccurred())
+					}(&wg)
 
-			fetcher := NewTokenFetcher(cfg)
-			_, err := fetcher.FetchToken()
-			Expect(err).To(HaveOccurred())
+					for i := 0; i < DefaultMaxNumberOfRetries; i++ {
+						fmt.Printf("Trying %d times\n", i)
+						Eventually(func() string {
+							r, err := ioutil.ReadAll(stdout)
+							log := string(r)
+							if err != nil {
+								return err.Error()
+							}
+							fmt.Println("printing log...", log)
+							fmt.Printf("Contains request:%t\n", strings.Contains(log, "REQUEST:"))
+							if !strings.Contains(log, "REQUEST:") {
+								return "No Request in log"
+							}
+							fmt.Printf("Contains POST:%t\n", strings.Contains(log, "POST /oauth/token HTTP/1.1"))
+							if !strings.Contains(log, "POST /oauth/token HTTP/1.1") {
+								return "No POST message in log"
+							}
+							fmt.Printf("ALL SUCCESS\n")
 
-			r, err := ioutil.ReadAll(stdout)
-			log := string(r)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(log).To(ContainSubstring("REQUEST: "))
-			Expect(log).To(ContainSubstring("POST /oauth/token HTTP/1.1"))
+							return ""
+						}, 5*time.Second, 1*time.Second).Should(Equal(""))
 
-			Expect(log).To(ContainSubstring("RESPONSE: "))
-			Expect(log).To(ContainSubstring("HTTP/1.1 400 Bad Request"))
+						clock.Increment(DefaultRetryInterval + 10*time.Second)
+					}
+					wg.Wait()
+				})
+			})
+
+			Context("when OAuth server returns a 5xx http status code", func() {
+				BeforeEach(func() {
+					server.AppendHandlers(
+						getOauthHandlerFunc(http.StatusServiceUnavailable, nil),
+						getOauthHandlerFunc(http.StatusInternalServerError, nil),
+						getOauthHandlerFunc(http.StatusBadGateway, nil),
+					)
+				})
+
+				It("retries a number of times", func() {
+					stdout := bytes.NewBuffer([]byte{})
+					trace.SetStdout(stdout)
+					trace.NewLogger("true")
+
+					fetcher, err := NewTokenFetcher(cfg, tokenFetcherConfig, clock)
+					Expect(err).NotTo(HaveOccurred())
+					verifyFetchWithRetries(fetcher, stdout, server, "HTTP/1.1 503", "HTTP/1.1 500", "HTTP/1.1 502")
+				})
+			})
+
+			Context("when OAuth server returns a 4xx http status code", func() {
+				It("does not retry", func() {
+
+				})
+			})
+
+		})
+
+		Context("when a new token needs to be fetched from OAuth server", func() {
+			Context("when the respose body is malformed", func() {
+				It("returns an error", func() {
+					stdout := bytes.NewBuffer([]byte{})
+					trace.SetStdout(stdout)
+					trace.NewLogger("true")
+
+					server.AppendHandlers(
+						ghttp.RespondWithJSONEncoded(http.StatusOK, "broken garbage response"),
+					)
+
+					fetcher, err := NewTokenFetcher(cfg, tokenFetcherConfig, clock)
+					Expect(err).NotTo(HaveOccurred())
+					_, err = fetcher.FetchToken(canUseCachedToken)
+					Expect(err).To(HaveOccurred())
+					Expect(server.ReceivedRequests()).Should(HaveLen(1))
+
+					verifyLogs(stdout, "POST /oauth/token HTTP/1.1", "HTTP/1.1 200 OK")
+				})
+			})
+
+			Context("when a non 200 OK is returned", func() {
+				It("returns an error", func() {
+					stdout := bytes.NewBuffer([]byte{})
+					trace.SetStdout(stdout)
+					trace.NewLogger("true")
+
+					server.AppendHandlers(
+						ghttp.RespondWith(http.StatusBadRequest, "you messed up"),
+					)
+
+					fetcher, err := NewTokenFetcher(cfg, tokenFetcherConfig, clock)
+					Expect(err).NotTo(HaveOccurred())
+					_, err = fetcher.FetchToken(canUseCachedToken)
+					Expect(err).To(HaveOccurred())
+					Expect(err.Error()).To(Equal("status code: 400, body: you messed up"))
+					Expect(server.ReceivedRequests()).Should(HaveLen(1))
+					verifyLogs(stdout, "POST /oauth/token HTTP/1.1", "HTTP/1.1 400 Bad Request")
+				})
+			})
+
+			Context("when there is no cached token", func() {
+				It("returns a new token and logs request response", func() {
+					responseBody := &Token{
+						AccessToken: "the token",
+						ExpireTime:  20,
+					}
+
+					server.AppendHandlers(
+						getOauthHandlerFunc(http.StatusOK, responseBody),
+					)
+
+					token, err := tokenFetcher.FetchToken(canUseCachedToken)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(server.ReceivedRequests()).Should(HaveLen(1))
+					Expect(token.AccessToken).To(Equal("the token"))
+					Expect(token.ExpireTime).To(Equal(int64(20)))
+				})
+
+				Context("when multiple goroutines fetch a token", func() {
+					It("contacts oauth server only once and returns cached token", func() {
+						responseBody := &Token{
+							AccessToken: "the token",
+							ExpireTime:  3600,
+						}
+
+						server.AppendHandlers(
+							getOauthHandlerFunc(http.StatusOK, responseBody),
+						)
+						wg := sync.WaitGroup{}
+						for i := 0; i < 2; i++ {
+							wg.Add(1)
+							go func(wg *sync.WaitGroup) {
+								defer GinkgoRecover()
+								defer wg.Done()
+								token, err := tokenFetcher.FetchToken(canUseCachedToken)
+								Expect(err).NotTo(HaveOccurred())
+								Expect(server.ReceivedRequests()).Should(HaveLen(1))
+								Expect(token.AccessToken).To(Equal("the token"))
+								Expect(token.ExpireTime).To(Equal(int64(3600)))
+							}(&wg)
+						}
+						wg.Wait()
+					})
+				})
+			})
+
+			Context("when cached token is expired", func() {
+				It("returns a new token and logs request response", func() {
+					firstResponseBody := &Token{
+						AccessToken: "the token",
+						ExpireTime:  3600,
+					}
+					secondResponseBody := &Token{
+						AccessToken: "another token",
+						ExpireTime:  3600,
+					}
+
+					server.AppendHandlers(
+						getOauthHandlerFunc(http.StatusOK, firstResponseBody),
+						getOauthHandlerFunc(http.StatusOK, secondResponseBody),
+					)
+
+					token, err := tokenFetcher.FetchToken(canUseCachedToken)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(server.ReceivedRequests()).Should(HaveLen(1))
+					Expect(token.AccessToken).To(Equal("the token"))
+					Expect(token.ExpireTime).To(Equal(int64(3600)))
+					clock.Increment((3600 - DefaultExpirationBufferTime) * time.Second)
+
+					token, err = tokenFetcher.FetchToken(canUseCachedToken)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(server.ReceivedRequests()).Should(HaveLen(2))
+					Expect(token.AccessToken).To(Equal("another token"))
+					Expect(token.ExpireTime).To(Equal(int64(3600)))
+				})
+			})
+		})
+
+		Context("when non expired token is cached", func() {
+			Context("when a cached token can be used", func() {
+				It("returns the cached token", func() {
+					firstResponseBody := &Token{
+						AccessToken: "the token",
+						ExpireTime:  3600,
+					}
+					secondResponseBody := &Token{
+						AccessToken: "another token",
+						ExpireTime:  3600,
+					}
+
+					server.AppendHandlers(
+						getOauthHandlerFunc(http.StatusOK, firstResponseBody),
+						getOauthHandlerFunc(http.StatusOK, secondResponseBody),
+					)
+
+					token, err := tokenFetcher.FetchToken(canUseCachedToken)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(server.ReceivedRequests()).Should(HaveLen(1))
+					Expect(token.AccessToken).To(Equal("the token"))
+					Expect(token.ExpireTime).To(Equal(int64(3600)))
+					clock.Increment(3000 * time.Second)
+
+					token, err = tokenFetcher.FetchToken(canUseCachedToken)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(server.ReceivedRequests()).Should(HaveLen(1))
+					Expect(token.AccessToken).To(Equal("the token"))
+					Expect(token.ExpireTime).To(Equal(int64(3600)))
+				})
+			})
+
+			Context("when a cached token cannot be used", func() {
+				It("returns a new token", func() {
+					firstResponseBody := &Token{
+						AccessToken: "the token",
+						ExpireTime:  3600,
+					}
+					secondResponseBody := &Token{
+						AccessToken: "another token",
+						ExpireTime:  3600,
+					}
+
+					server.AppendHandlers(
+						getOauthHandlerFunc(http.StatusOK, firstResponseBody),
+						getOauthHandlerFunc(http.StatusOK, secondResponseBody),
+					)
+
+					token, err := tokenFetcher.FetchToken(canUseCachedToken)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(server.ReceivedRequests()).Should(HaveLen(1))
+					Expect(token.AccessToken).To(Equal("the token"))
+					Expect(token.ExpireTime).To(Equal(int64(3600)))
+
+					token, err = tokenFetcher.FetchToken(false)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(server.ReceivedRequests()).Should(HaveLen(2))
+					Expect(token.AccessToken).To(Equal("another token"))
+					Expect(token.ExpireTime).To(Equal(int64(3600)))
+				})
+			})
+
 		})
 	})
 })
